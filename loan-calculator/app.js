be3ab23b7e9d27bc51b3886683d689c19afaec0d
Kyle Desktop <kylespresser@gmail.com>
2018-08-22 10:57:17 -0400

    lecture 38

--- loan-calculator/app.js
+++ loan-calculator/app.js
@@ -1,8 +1,18 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults)
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log('calculating...');
   // UI variables
   const amount = document.getElementById('amount');
@@ -18,23 +28,33 @@ function calculateResults(e) {
 
   // Compute monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal * x * calculatedInterest)/(x-1);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
 
-  if(isFinite(monthly)) {
+  if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
-    showError('Please check your numbers')
+    showError('Please check your numbers');
   }
 
-  e.preventDefault();
+  // e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create div
-  const errorDiv = document.createElement('div'); 
+  const errorDiv = document.createElement('div');
 
   // Get elements
   const card = document.querySelector('.card');
